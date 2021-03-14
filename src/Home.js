956b7c7c276b698953017b691bd52e15a133e0f6
Bruce Tuhakaraina <Bruce.Tuhakaraina@toiohomai.ac.nz>
2021-03-14 14:52:00 +1300

    This commit has entire node server modules including react router package, no need to use create-react-app

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Home extends Component 
+{
+         render ()
+         {
+             return(
+                <div>
+                   <h2>Welcome to Home Component.</h2>
+                   <p>This is content for the home component</p>
+                </div>
+             );
+         }
+}
+export default Home;
---
+++ src/Stuff.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Stuff extends Component 
+{
+         render ()
+         {
+             return(
+                <div>
+                   <h2>Stuff Component</h2>
+                   <p>Content for Stuff.</p>
+                   <ol>
+                       <li>One</li>
+                       <li>Two</li>
+                       <li>Three</li>
+                   </ol>
+                </div>
+             );
+         }
+}
+export default Stuff;
